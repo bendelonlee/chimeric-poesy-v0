58b2c44b1f44e6a8618cb741e73f8f1d3ee2e092
Benjamin Lee <benjaminlee@Benjamins-MacBook-Pro.local>
2024-03-29 21:13:12 -0500

    env

--- src/hooks/useSession.js
+++ src/hooks/useSession.js
@@ -8,7 +8,7 @@ export function useSession(pagename) {
 
 
 export function onLoad(pagename){
-    fetch("http://localhost:3000/api/v1/sessions", {
+    fetch(`${process.env.BACKEND}/api/v1/sessions`, {
         method: "POST",
         body: JSON.stringify({
             site_name: "chimeric-poesy.com",
@@ -41,7 +41,7 @@ function schedulePing(id) {
   }
 
 function ping(id){
-    fetch(`http://localhost:3000/api/v1/sessions/${id}`, {
+    fetch(`${env.BACKEND}/api/v1/sessions/${id}`, {
         method: "PUT",
         headers: {
             "Content-type": "application/json; charset=UTF-8"
